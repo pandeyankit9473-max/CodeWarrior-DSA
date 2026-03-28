3aaa707a446310804363cb9ee12544191cfcf470
Ankit kumar <pandeyankit9473@gmail.com>
2026-03-28 23:37:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (71.75%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
         int n=arr.size();
-        for(int i=0;i<n;i++){
-            if(((i==0) || arr[i-1]<arr[i])&&
-            ((i==n-1) || arr[i]>arr[i+1]))
-            return i;
+        int low=0 , high=n-1;
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]<arr[mid+1]) // increasing curve 
+            low=mid+1; // peak on right side
+            else
+            high=mid; // either mid is peak or else peak on left side 
         }
-        return -1;
+       return low;
     }
 };
+
+//  for(int i=0;i<n;i++){
+//             if(((i==0) || arr[i-1]<arr[i])&&
+//             ((i==n-1) || arr[i]>arr[i+1]))
+//             return i;
+//         }
+//         return -1;
