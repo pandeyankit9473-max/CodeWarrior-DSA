605d2f22342707c011f482712265c29453c75f0b
Ankit kumar <pandeyankit9473@gmail.com>
2026-04-03 01:40:24 +0530

    Time: 0 ms (100%), Space: 96.1 MB (9.6%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        vector<int> merged;
-        
-        int i = 0, j = 0;
-        
-        while(i < nums1.size() && j < nums2.size()) {
-            if(nums1[i] < nums2[j]) {
-                merged.push_back(nums1[i]);
-                i++;
-            }
-            else {
-                merged.push_back(nums2[j]);
-                j++;
-            }
+        int n1=nums1.size();
+        int n2=nums2.size();
+        vector<int>a;
+        int i=0 ,j=0;
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]) a.push_back(nums1[i++]);
+            else
+            a.push_back(nums2[j++]);
         }
+        while(i<n1) a.push_back(nums1[i++]);
+            while(j<n2) a.push_back(nums2[j++]);
+        int n=a.size();
+        if(n%2==1) return a[n/2];
+        else 
+        return (a[n/2]+a[n/2-1])/2.0;
         
-        while(i < nums1.size()) {
-            merged.push_back(nums1[i]);
-            i++;
-        }
-        
-        while(j < nums2.size()) {
-            merged.push_back(nums2[j]);
-            j++;
-        }
-        
-        int n = merged.size();
-        
-        if(n % 2 == 1)
-            return merged[n/2];
-        else
-            return (merged[n/2] + merged[n/2 - 1]) / 2.0;
     }
 };
+// one mistake i am doing here like if i declare a vector array with the size its not need to use a.push back waala mujhe index use karni chaiye ya normally size hata do
