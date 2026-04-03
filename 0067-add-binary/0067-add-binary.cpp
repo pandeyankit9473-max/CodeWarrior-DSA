eba1c21a484c9daf28ed8156dd3376db81cfdbd3
Ankit kumar <pandeyankit9473@gmail.com>
2026-04-03 23:51:14 +0530

    Time: 4 ms (7.87%), Space: 8.9 MB (82.86%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i=a.size()-1;
+        int j=b.size()-1;
+        int carry=0 ,sum=0;
+        string result="";
+        while(i>=0 || j>=0){
+         sum=carry;
+            if(i>=0){
+                sum+=a[i]-'0';
+                i--;
+            }
+            if(j>=0){
+                sum+=b[j]-'0';
+                j--;
+            }
+            result.push_back((sum%2)==0 ?'0':'1');
+            carry=sum/2;
+        }
+        if(carry)
+        result.push_back('1');
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
