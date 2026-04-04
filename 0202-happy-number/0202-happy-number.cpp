a04506c93a1098a2b5659a02d11467bda39dd70a
Ankit kumar <pandeyankit9473@gmail.com>
2026-04-05 01:22:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (6.02%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-
-    int squareSum(int n){
-        int sum = 0;
-
-        while(n > 0){
-            int digit = n % 10;
-            sum += digit * digit;
-            n = n / 10;
-        }
-
-        return sum;
-    }
-
     bool isHappy(int n) {
 
         unordered_set<int> seen;
 
         while(n != 1){
 
+            int sum = 0;
+
+            while(n > 0){
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+
+            n = sum;
+
             if(seen.count(n)) return false;
 
             seen.insert(n);
-
-            n = squareSum(n);
         }
 
         return true;
