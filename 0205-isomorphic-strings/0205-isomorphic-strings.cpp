5fb21542f98fbbcd7aa614b5f49c11b5e480da8e
Ankit kumar <pandeyankit9473@gmail.com>
2026-04-07 20:20:01 +0530

    Time: 3 ms (39.83%), Space: 9.4 MB (27.31%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        
+        unordered_map<char ,char>mp1;
+        unordered_map<char ,char>mp2;
 
-        int n = s.size();
-
-        for(int i = 0; i < n; i++){
-
-            for(int j = i + 1; j < n; j++){
-
-                if(s[i] == s[j] && t[i] != t[j])
-                    return false;
-
-                if(t[i] == t[j] && s[i] != s[j])
-                    return false;
-            }
+        for(int i=0;i<s.size();i++){
+            char a=s[i];
+            char b=t[i];
+            if(mp1.count(a) && mp1[a]!=b)
+            return false;
+            if(mp2.count(b)&& mp2[b]!=a)
+            return false;
+            mp1[a]=b;
+            mp2[b]=a;
         }
 
-        return true;
+
+       return true;
     }
 };
+//  int n = s.size();
+
+//         for(int i = 0; i < n; i++){
+
+//             for(int j = i + 1; j < n; j++){
+
+//                 if(s[i] == s[j] && t[i] != t[j])
+//                     return false;
+
+//                 if(t[i] == t[j] && s[i] != s[j])
+//                     return false;
+//             }
+//         }
+
+//         return true;
