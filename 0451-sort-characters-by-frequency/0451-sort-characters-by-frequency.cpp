d5646b90e8ffb00de513796912fdfc06691b7545
Ankit kumar <pandeyankit9473@gmail.com>
2026-04-10 00:23:20 +0530

    Time: 0 ms (100%), Space: 10.7 MB (88.22%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -4,8 +4,8 @@ public:
         vector<pair<char,int>>v(123);
         for(int i=0;i<s.size();i++){
             char ch=s[i];
-            v[ch].first=ch;
-            v[ch].second++;
+            v[ch].first=ch; // its character for ex for 't' at v[ch]=116..at 116
+            v[ch].second++; // its freq at 116 now(t,1)
         }
         auto lambda= [&](pair<char,int>&a ,pair<char,int>b){
             return a.second>b.second;
@@ -15,9 +15,16 @@ public:
         for(int i=0;i<123;i++){
             char ch=v[i].first;
             int frq=v[i].second;
-            for(int i=0;i<frq;i++)
+            for(int j=0;j<frq;j++)
             res+=ch;
         }
         return res;
     }
 };
+//ASCII vector → frequency count → sort by freq → repeat char freq times..
+// v[116].first = 't'
+// v[116].second++
+
+// Vector state:
+
+// 116 → ('t',1)
