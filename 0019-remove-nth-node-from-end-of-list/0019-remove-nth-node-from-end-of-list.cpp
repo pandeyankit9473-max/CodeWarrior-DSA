c224e31583d569dfc29805d04192582831b7f22b
Ankit kumar <pandeyankit9473@gmail.com>
2026-04-16 22:40:15 +0530

    Time: 0 ms (100%), Space: 15 MB (58.25%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,24 +11,42 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp=head;
-        int cnt=0;
-        if(temp==NULL) return head;
-        while(temp!=NULL){
-            cnt++;
-            temp=temp->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        // first move fast the given n steps;
+        for(int i=0;i<n;i++) fast=fast->next;
+        //edge case when it said to delete head...and fast became null;
+        if(fast==NULL) return head->next; //return node ater head and so on...
+        // now move both together...
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
         }
-        if(n == cnt){
-         return head->next;
-         }
-       int count=0; temp=head;
-        while(temp!=NULL){
-            count++;
-            if(count==cnt-n)
-            temp->next=temp->next->next;
-            else
-            temp=temp->next;
-        }
+        // now boom slow will be just before of the node that we have to delete..
+        slow->next=slow->next->next;
         return head;
+
     }
 };
+// ListNode* temp=head;
+//         int cnt=0;
+//         if(temp==NULL) return head;
+//         while(temp!=NULL){
+//             cnt++;
+//             temp=temp->next;
+//         }
+//         if(n == cnt){ // edge case handling when only one element in list..
+//          return head->next;
+//          }
+//        int count=0; temp=head;
+//         while(temp!=NULL){
+//             count++;
+//             if(count==cnt-n)
+//             {
+//             temp->next=temp->next->next;
+//             break;
+//             }
+//             else
+//             temp=temp->next;
+//         }
+//         return head;
