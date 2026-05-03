875879fbfaaf487c0eee8a82d204fe816c77e2ec
Ankit kumar <pandeyankit9473@gmail.com>
2026-05-04 01:25:49 +0530

    Time: 0 ms (100%), Space: 8.3 MB (24.83%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size())
-    return false;
+        return false; // as it is rotation len of both str must be same..
         string a=s.append(s);
         if(a.find(goal)!=string::npos)
         return true;
