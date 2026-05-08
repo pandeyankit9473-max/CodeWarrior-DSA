21a0a0d8446d1d21d7e394135b8b6e5a931ae1ab
Ankit kumar <pandeyankit9473@gmail.com>
2026-05-09 02:24:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (19.72%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0) sum+=i;
+        if(num==1) return false; //we have to exclude the itself no..
+        int sum=1;
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+            sum+=i;
+            if(i!=num/i) // to remove duplicates 
+                sum+=num/i;
+            }
         }
         return sum==num;
     }
 };
+// int sum=0;
+//         for(int i=1;i<=num/2;i++){
+//             if(num%i==0) sum+=i;
+//         }
+//         return sum==num;
